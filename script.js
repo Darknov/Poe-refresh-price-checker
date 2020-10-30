2a274b2b8ee11504db32b848c7049acb919685bb
Przemysław Maciaszek <darknov.dono@gmail.com>
2020-10-30 21:37:32 +0100

    Final commit

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const price = document.querySelector('.price');
+const time = document.querySelector('.time');
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const refreshTime = 301;
+
+let timeLeft = refreshTime;
 
 function updatePrice() {
 	fetch(proxyurl+ 'https://poe.ninja/api/data/CurrencyHistory?league=Heist&type=Currency&currencyId=2')
 		.then(response => response.json())
 		.then(data => {
 			const prices = data.receiveCurrencyGraphData;
-			price.textContent = prices[prices.length - 1].value;
+			price.textContent = Math.round(prices[prices.length - 1].value * 10) / 10;
 		});
+	timeLeft = refreshTime;
 }
 
 updatePrice();
-setInterval( updatePrice, 300000);
+setInterval(() => {
+	timeLeft = timeLeft - 1;
+	time.textContent = timeLeft;
+}, 1000);
+setInterval( updatePrice, refreshTime * 1000);
