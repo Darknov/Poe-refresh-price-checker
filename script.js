bc4c0cd041b02fd163fcb3e9a824d1e35d359b33
Przemysław Maciaszek <darknov.dono@gmail.com>
2020-10-30 21:17:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const price = document.querySelector('.price');
 
 
 function updatePrice() {
-	fetch('https://poe.ninja/api/data/CurrencyHistory?league=Heist&type=Currency&currencyId=2',	{mode: 'no-cors'})
-		.then(response => response.json())
+	fetch('https://poe.ninja/api/data/CurrencyHistory?league=Heist&type=Currency&currencyId=2',	{
+		accept: 'application/json',
+		referrer: 'https://poe.ninja/challenge/currency/exalted-orb'})
+		.then(response => JSON.parse(response))
 		.then(data => {
 			const prices = data.receiveCurrencyGraphData;
 			price.textContent = prices[prices.length - 1].value;
